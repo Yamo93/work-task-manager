4938bcfcd7203534a66fe51bb4e6ab515c875247
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2021-06-12 22:05:24 +0200

    Add dashboard view

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,5 +1,60 @@
 import React, { ReactElement } from 'react';
+import {
+  Container,
+  Box,
+  Center,
+  Heading,
+  TabList,
+  Tabs,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from '@chakra-ui/react';
+import os from 'os';
+import s from './Dashboard.scss';
+import TabView from '../TabView/TabView';
 
 export default function Dashboard(): ReactElement {
-  return <div>Welcome to the Dashboard.</div>;
+  const { username } = os.userInfo();
+  return (
+    <div className={s.dashboard}>
+      <Container maxW="container.xl">
+        <Tabs
+          size="md"
+          variant="soft-rounded"
+          colorScheme="green"
+          align="center"
+        >
+          <TabList>
+            <Tab>Home</Tab>
+            <Tab>Work check-in</Tab>
+            <Tab>Workspaces</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <TabView>
+                <Box textStyle="h2">
+                  <Center>
+                    <Heading fontSize="2xl">
+                      Welcome to the Dashboard, {username}! 🙂
+                    </Heading>
+                  </Center>
+                </Box>
+              </TabView>
+            </TabPanel>
+            <TabPanel>
+              <TabView>
+                <Container>Work check-in</Container>
+              </TabView>
+            </TabPanel>
+            <TabPanel>
+              <TabView>
+                <Container>Workspaces</Container>
+              </TabView>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Container>
+    </div>
+  );
 }
