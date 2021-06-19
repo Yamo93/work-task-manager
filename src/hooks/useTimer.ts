c39ecc81a3ee98d054155d8099a52f276cef79fd
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2021-06-19 19:13:33 +0200

    Fix time bug

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -65,8 +65,8 @@ export default function useTimer(): HookResult {
 
   function formatTime(time: number = workTime): string {
     const seconds = `0${time % 60}`.slice(-2);
-    const minutes = `0${Math.floor(workTime / 60) % 60}`.slice(-2);
-    const hours = `0${Math.floor(workTime / 3600)}`.slice(-2);
+    const minutes = `0${Math.floor(time / 60) % 60}`.slice(-2);
+    const hours = `0${Math.floor(time / 3600)}`.slice(-2);
 
     return `${hours}:${minutes}:${seconds}`;
   }
