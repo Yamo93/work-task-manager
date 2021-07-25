6f0bd8b900d72f422a448bcc3df6c6cd4f76ea51
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2021-07-25 22:27:45 +0200

    Add application name for installation distribution

--- src/menu.ts
+++ src/menu.ts
@@ -46,14 +46,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Work Task Manager',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Work Task Manager',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
